5d7b50b41111c27ae4c1cf4976be868a65582815
Carlos Eduardo Oliveira <carlos.oliveira@platformbuilders.io>
2026-03-23 17:40:59 -0300

    Estrutura de projeto Main->Modules base

---
+++ src/command-cashflow-service/src/main/java/com/ciandt/challenge/query_cashflow_service/QueryCashflowServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ciandt.challenge.query_cashflow_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryCashflowServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryCashflowServiceApplication.class, args);
+	}
+
+}
---
+++ src/query-cashflow-service/src/test/java/com/ciandt/challenge/query_cashflow_service/QueryCashflowServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciandt.challenge.query_cashflow_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryCashflowServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
