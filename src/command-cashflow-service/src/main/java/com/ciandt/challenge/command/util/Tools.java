3c55a0053eb9294696e904324cc1a6faf19f8b1c
Carlos Eduardo Oliveira <carlos.oliveira.mobile@gmail.com>
2026-03-24 17:35:21 -0300

    Finalizado envio do evento para tópico de consolidado

--- src/command-cashflow-service/src/main/java/com/ciandt/challenge/command/util/Tools.java
+++ src/command-cashflow-service/src/main/java/com/ciandt/challenge/command/util/Tools.java
@@ -1,12 +1,23 @@
 package com.ciandt.challenge.command.util;
 
 import com.ciandt.challenge.shared.domain.FinancialRecord;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.google.gson.Gson;
 
 public class Tools {
 
-    public static String toJson(FinancialRecord record){
-        return new Gson().toJson(record);
+    private static final ObjectMapper MAPPER = new ObjectMapper()
+            .registerModule(new JavaTimeModule())
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+    public static String toJson(Object obj) {
+        try {
+            return MAPPER.writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao serializar objeto para JSON", e);
+        }
     }
 
     public static FinancialRecord toFinancialRecord( String json){
